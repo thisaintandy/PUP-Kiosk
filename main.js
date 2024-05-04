d778d5d11e14aa0ee07d01bb0a6cb172c87fca08
thisaintandy_ <andydymasarque@gmail.com>
2024-05-05 07:03:41 +0800

    Updated the files. Added some areas in the map. Edited design of the div

--- main.js
+++ main.js
@@ -1,47 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var areas = document.querySelectorAll('map area');
+
+    areas.forEach(function(area) {
+        area.addEventListener('mouseover', function() {
+            var detailsDiv = document.getElementById('details');
+            var areaName = area.getAttribute('alt');
+            var areaDescription = area.getAttribute('data-description');
+            var areaImage = area.getAttribute('data-img');
+
+            var areaNameElement = document.getElementById('area-name');
+            var imageElement = document.getElementById('area-image');
+            var descriptionElement = document.getElementById('description');
+
+            areaNameElement.textContent = areaName;
+            imageElement.src = areaImage;
+            descriptionElement.textContent = areaDescription;
+
+            detailsDiv.style.display = 'block'; // Show details div
+        });
+
+        area.addEventListener('mouseout', function() {
+            var detailsDiv = document.getElementById('details');
+            detailsDiv.style.display = 'none'; // Hide details div
+        });
+    });
+});
+
+
 function showSceneryBox(imgSource, heading, description) {
-    let dis = document.getElementById("display");
-    let bod = document.getElementById("body");
-
-    bod.style.overflow = "hidden";
-    document.addEventListener('mousemove', displayFollowCamera);
-    dis.style.left = event.clientX + 5 + "px";
-    dis.style.top = event.clientY - dis.offsetHeight - 5 + "px"; // Subtract div height from Y-coordinate
-    dis.innerHTML = "<div id='wrapper'><img id='imgid' src='" + imgSource + "'>" + "<p>" + heading + "</p>" + "<p>" + description + "</p></div>";
-}
-
-function sceneryBoxNoImg(heading, description) {
-    let dis = document.getElementById("display");
-    let bod = document.getElementById("body");
-
-    bod.style.overflow = "hidden";
-    document.addEventListener('mousemove', displayFollowCamera);
-    dis.style.left = event.clientX + 5 + "px";
-    dis.style.top = event.clientY - dis.offsetHeight - 5 + "px"; // Subtract div height from Y-coordinate
-    dis.innerHTML = "<div id = 'wrapper2'>" + "<p>" + heading + "</p>" + description + "</p></div>"
-}
-
-function showLongText(imgSource, heading, description) {
-    let dis = document.getElementById("display");
-    let bod = document.getElementById("body");
-
-    bod.style.overflow = "hidden";
-    document.addEventListener('mousemove', displayFollowCamera);
-    dis.style.left = event.clientX + 5 + "px";
-    dis.style.top = event.clientY - dis.offsetHeight - 5 + "px"; // Subtract div height from Y-coordinate
-    dis.innerHTML = "<div id='wrapper3'><img id='imgid' src='" + imgSource + "'>" + "<p>" + heading + "</p>" + "<p>" + description + "</p></div>";
-}
-
-function displayFollowCamera() {
-    let b = document.getElementById('display');
-    b.style.left = event.clientX + 5 + "px";
-    b.style.top = event.clientY - b.offsetHeight - 5 + "px"; // Subtract div height from Y-coordinate
-}
-
-function removeSceneryBox() {
-    let c = document.getElementById('display');
-    let d = document.getElementById('body');
-    c.removeAttribute('style');
-    d.removeAttribute('style');
-    document.removeEventListener('mousemove', displayFollowCamera);
-    c.removeChild(c.childNodes[0]);
+    let dis = document.getElementById("details");
+    dis.innerHTML = "<img src='" + imgSource + "' alt='" + heading + "'>";
+    dis.innerHTML += "<h3>" + heading + "</h3>";
+    dis.innerHTML += "<p>" + description + "</p>";
 }
